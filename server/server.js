07c7231bfeabd24fde2e95d7be8e70a5fd50a37e
kwankhede <kapilwankhede22@gmail.com>
2023-07-15 15:55:53 +0530

    updated baseurl

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path - require("path/posix")
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -81,15 +81,21 @@ app.put("/questions/:id", async (req, res) => {
   }
 });
 
-//production Script 
+//production Script
 app.use(express.static("./client/build"));
 
-app.get("*",(req, res) =>{
-  res.sendFile(path.resolve(__dirname,"client", "build","index.html"))
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const port = process.env.PORT || 5001;
+
+//production script
+
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  return res.sendFile("./client/build/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
